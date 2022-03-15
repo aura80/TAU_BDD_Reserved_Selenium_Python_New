b103dc78e95cf3b26c8770e98b53a1c97f26e0df
aura80 <96388713+aura80@users.noreply.github.com>
2022-03-15 12:36:54 +0200

    BDD change passwords from "test_register_page.feature"
    The second password must have 5 characters in front of @

--- tests/step_defs/test_search_steps.py
+++ tests/step_defs/test_search_steps.py
@@ -10,7 +10,7 @@ def open_page(browser):
     negative.load_page()
 
 @when(parsers.cfparse('we wrote haine "{haine}" in the search field'))
-@when('we wrote pijama "<haine>" in the search field')
+@when('we wrote haine "<haine>" in the search field')
 def check_email(browser, haine):
     negative = SearchTests(browser)
     negative.search_pijama_click(haine)
